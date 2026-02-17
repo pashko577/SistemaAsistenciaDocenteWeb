766ff475a7da3d8c4b98c8919255c3d9b41137ae
pashko577 <tonnyghp577@gmail.com>
2026-02-16 20:00:45 -0500

    subiendo idpersona e idsede a usuario fk

--- BACK-actividades-app/src/main/java/com/example/actividades_app/model/Usuario.java
+++ BACK-actividades-app/src/main/java/com/example/actividades_app/model/Usuario.java
@@ -25,29 +25,34 @@ public class Usuario {
     @Column(name = "password", nullable = false)
     private String password;
 
-    //Conexion con tabla intermedio
-    @ManyToMany(fetch = FetchType.EAGER,targetEntity = Rol.class, cascade = CascadeType.PERSIST)
-    @JoinTable(
-        name = "usuario_rol",
-        joinColumns = @JoinColumn(name = "usuario_id"),
-        inverseJoinColumns = @JoinColumn(name = "rol_id")
-    )
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "personaID", nullable = false)
+    private Persona persona;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "sedeID", nullable = false)
+    private Sede sede;
+
+    @ManyToMany(fetch = FetchType.EAGER, targetEntity = Rol.class, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"))
     private Set<Rol> roles;
 }
-/* 
-    //Conexion ProyectoUsuario
-    @OneToMany(
-        mappedBy = "usuario",
-        cascade = CascadeType.ALL,
-        orphanRemoval = false
-    )
-    private List<ProyectoUsuario> proyectos;
-
-    //Conexion UsuarioActividad
-    @OneToMany(
-        mappedBy = "usuario",
-        cascade = CascadeType.ALL,
-        orphanRemoval = false
-    )
-    private List<UsuarioActividad> actividades;
-    */
+/*
+ * //Conexion ProyectoUsuario
+ * 
+ * @OneToMany(
+ * mappedBy = "usuario",
+ * cascade = CascadeType.ALL,
+ * orphanRemoval = false
+ * )
+ * private List<ProyectoUsuario> proyectos;
+ * 
+ * //Conexion UsuarioActividad
+ * 
+ * @OneToMany(
+ * mappedBy = "usuario",
+ * cascade = CascadeType.ALL,
+ * orphanRemoval = false
+ * )
+ * private List<UsuarioActividad> actividades;
+ */
